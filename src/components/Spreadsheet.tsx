4a3527e1e75426082e32891d6922707054c2e67c
Cosi Leone <cosi.leone@icloud.com>
2024-08-22 14:50:12 +0100

    add row numbering

--- src/components/Spreadsheet.tsx
+++ src/components/Spreadsheet.tsx
@@ -1,11 +1,11 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import _ from 'lodash';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Cell from 'components/Cell';
 
-const NUM_ROWS = 10;
-const NUM_COLUMNS = 10;
+const NUM_ROWS = 20;
+const NUM_COLUMNS = 20;
 
 const Spreadsheet: React.FC = () => {
   const [spreadsheetState, setSpreadsheetState] = useState(
@@ -13,27 +13,52 @@ const Spreadsheet: React.FC = () => {
   );
 
   return (
-    <Box width="full">
+    <Box width="full" ml="8">
       {spreadsheetState.map((row, rowIdx) => {
         return (
-          <Flex key={String(rowIdx)}>
+          <Flex key={String(rowIdx)} position="relative">
+            <Text
+              key={String(rowIdx)}
+              color="gray.400"
+              fontWeight="400"
+              fontSize="sm"
+              position="absolute"
+              left="-2rem"
+              top="0.5rem"
+              textAlign="right"
+            >
+              {rowIdx + 1}
+            </Text>
             {row.map((cellValue, columnIdx) => (
-              <Cell
-                key={`${rowIdx}/${columnIdx}`}
-                value={cellValue}
-                onChange={(newValue: string) => {
-                  const newRow = [
-                    ...spreadsheetState[rowIdx].slice(0, columnIdx),
-                    newValue,
-                    ...spreadsheetState[rowIdx].slice(columnIdx + 1),
-                  ];
-                  setSpreadsheetState([
-                    ...spreadsheetState.slice(0, rowIdx),
-                    newRow,
-                    ...spreadsheetState.slice(rowIdx + 1),
-                  ]);
-                }}
-              />
+              <Fragment key={`${rowIdx}/${columnIdx}`}>
+                {rowIdx === 0 ?? (
+                  <Text
+                    color="gray.400"
+                    fontWeight="400"
+                    fontSize="sm"
+                    position="absolute"
+                    top="-2rem"
+                    textAlign="right"
+                  >
+                    shenanigans
+                  </Text>
+                )}
+                <Cell
+                  value={cellValue}
+                  onChange={(newValue: string) => {
+                    const newRow = [
+                      ...spreadsheetState[rowIdx].slice(0, columnIdx),
+                      newValue,
+                      ...spreadsheetState[rowIdx].slice(columnIdx + 1),
+                    ];
+                    setSpreadsheetState([
+                      ...spreadsheetState.slice(0, rowIdx),
+                      newRow,
+                      ...spreadsheetState.slice(rowIdx + 1),
+                    ]);
+                  }}
+                />
+              </Fragment>
             ))}
           </Flex>
         );
